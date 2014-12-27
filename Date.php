357b8e6b1ffd9dbf09ed0cdabbeb4f066ce8f3be
Alexander Gamaunov <unlike777@gmail.com>
2014-12-27 13:53:23 +0600

    Исправлена ошибка определения дня недели

--- Date.php
+++ Date.php
@@ -97,7 +97,7 @@ class Date {
 	 */
 	public function format($format = 'Y-m-d H:i:s') {
 
-		$num_day = date('w', self::$time) + 1;
+		$num_day = date('N', self::$time);
 		self::replace($format, $num_day, array(
 			'ddd' => 'fweekday',
 			'dd' => 'sweekday',
